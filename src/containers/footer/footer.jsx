60d388d249e9db947c0c06a72a41297542d2943c
Xi Gao <xiygao@gmail.com>
2023-03-04 20:53:22 -0700

    Updating footer container

--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
-    <div>
-        Footer
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'> Do you want to step into the future</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request early access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo' />
+          <p>123 Street, City, State 01234, All Rights Reserved</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>123 Street, City, State 01234</p>
+          <p>123-456-7890</p>
+          <p>info@email.com</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>@2023 GPT-3. All rights reserved.</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
